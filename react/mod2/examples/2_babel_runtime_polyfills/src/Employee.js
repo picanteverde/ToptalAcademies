0bbd3f2990cc55242d0b709a6215d8dd68575a89
Alejandro Hernandez <picanteverde@gmail.com>
2016-04-07 01:48:27 -0300

    classes fix

--- react/mod2/examples/2_babel_runtime_polyfills/src/Employee.js
+++ react/mod2/examples/2_babel_runtime_polyfills/src/Employee.js
@@ -2,8 +2,9 @@ import Person from 'Person';
 
 export default class Employee extends Person{
   constructor(options){
+    super(options);
     this.name = 'Employee' + options.id;
-  },
+  }
   work(){
     console.log(`${this.name} working!`);
   }
--- react/mod2/examples/2_babel_runtime_polyfills/src/Person.js
+++ react/mod2/examples/2_babel_runtime_polyfills/src/Person.js
@@ -1,7 +1,7 @@
 export default class Person {
   constructor(options){
     this.name = 'Person' + options.id;
-  },
+  }
   sleep(){
     console.log(`${this.name} is sleeping!`);
   }
